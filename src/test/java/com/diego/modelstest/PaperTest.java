cddae1be07daaf0178c70d1f27345c5dfe51917f
Cortina17 <diego.cortinagarcia@gmail.com>
2022-02-18 19:03:33 +0100

    modified pom

--- src/test/java/com/diego/modelstest/PaperTest.java
+++ src/test/java/com/diego/modelstest/PaperTest.java
@@ -1,5 +1,24 @@
 package com.diego.modelstest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.diego.models.Paper;
+
+import org.junit.jupiter.api.Test;
+
+
 public class PaperTest {
+
+    @Test
+    void create_paper() {
+        Object paper = new Paper();
+        assertTrue(paper instanceof Paper);
+    }
+
+    @Test
+    void test_paper_is_paper() {
+        Object paper = new Paper();
+        assertTrue("Paper", true);
+    }
 
 }
